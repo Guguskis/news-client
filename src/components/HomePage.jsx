7de6ac66ee1187276b2ccce2b8bf3492241beaf5
Guguskis <matasliutikas@gmail.com>
2021-12-05 00:56:54 +0200

    added button add signal

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,20 +4,37 @@ import React from 'react';
 
 // import typography
 import Typography from '@material-ui/core/Typography';
+import Button from '@mui/material/Button';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
-const HomePage = () => (
+function HomePage()  {
+
+    function addSignal() {
+        return console.log('add signal');
+    }
 
 
+    return (
+        <>
+            <Typography
+                variant="h6"
+                component="h1"
+                color="textPrimary"
+                gutterBottom
+            >
+                Signals
+            </Typography>
 
-    <div>
-        <Typography
-            variant="h6"
-            component="h1"
-            color="textPrimary"
-        >
-            Signals
-        </Typography>
-    </div>
-);
+            <Button
+                onClick={() => addSignal()}
+                variant="contained"
+                endIcon={<AddBoxIcon />}
+                type="submit"
+            >
+                Create
+            </Button>
+        </>
+    );
+};
 
 export default HomePage;
