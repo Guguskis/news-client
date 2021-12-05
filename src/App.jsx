a88b4ea03a120b1a993f119b572b7a86793cb0d4
Guguskis <matasliutikas@gmail.com>
2021-12-05 21:43:10 +0200

    fixed background dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import HomePage from './components/HomePage.jsx';
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import { Box } from '@mui/material/Box';
 
 const theme = createTheme({
   palette: {
@@ -21,10 +23,18 @@ const theme = createTheme({
   },
 });
 
+
 function App() {
+  // add dark background color to Box component 
+  // <Box sx={{border: "1px solid red" }}>
+  // </Box>
+
+
+
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
       <Router>
         <Switch>
           <Route exact path="/" component={HomePage} />
