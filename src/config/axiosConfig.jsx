4ba4077e13d17e2cec282f93412ccf52afcbfda6
Guguskis <matasliutikas@gmail.com>
2022-03-10 17:30:43 +0200

    news history noew loads on startup

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -2,12 +2,17 @@ import { makeUseAxios } from "axios-hooks";
 import axios from "axios";
 
 const CRYPTO_API_URL = "http://86.100.240.140:9081";
+const NEWS_API_URL = "http://86.100.240.140:9081";
 
 const cryptoApiAxios = axios.create({ baseURL: CRYPTO_API_URL });
+const newsApiAxios = axios.create({ baseURL: NEWS_API_URL });
 
 const API = {
     useCryptoApi: makeUseAxios({
         axios: cryptoApiAxios
+    }),
+    useNewsApi: makeUseAxios({
+        axios: newsApiAxios
     }),
 }
 
--- src/hooks/useNewsClient.jsx
+++ src/hooks/useNewsClient.jsx
@@ -1,18 +1,34 @@
 import { useState, useEffect } from 'react';
 
-import {
-  useSubscription,
-} from "react-stomp-hooks";
+import { useSubscription } from "react-stomp-hooks";
 import { ArraysState } from '../utils/utils.jsx';
+
+import { API } from '../config/axiosConfig.jsx';
 
 export function useNewsClient() {
   const [news, setNews] = useState([]);
   const [message, setMessage] = useState("");
+  const [{ data: getNewsData, loading: getNewsLoading, error: getNewsError }, getNewsExecute] = API.useNewsApi({
+    url: "/api/news",
+    method: "GET"
+  })
 
   useSubscription("/topic/news", (message) => setMessage(message.body));
 
   useEffect(() => {
-    if (message === "") 
+    if (!getNewsLoading && getNewsData) {
+      let news = getNewsData.news.map(newsItem => {
+        newsItem.created = new Date(newsItem.created);
+        return newsItem;
+      })
+      news.sort((a, b) => a.created - b.created);
+
+      ArraysState.add(setNews, news);
+    }
+  }, [getNewsLoading, getNewsData]);
+
+  useEffect(() => {
+    if (message === "")
       return;
 
     try {
