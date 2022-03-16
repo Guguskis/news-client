9aca80e85384d5baba03600c5a81c1f61f27f702
Guguskis <matasliutikas@gmail.com>
2022-03-16 20:54:15 +0200

    added boilerplate for subscription form

--- src/page/MainPage.jsx
+++ src/page/MainPage.jsx
@@ -1,9 +1,11 @@
 import React, { useCallback, useEffect } from 'react'
 import { useNewsClient } from '../hooks/useNewsClient.jsx'
-import { Box, Container, LinearProgress, Button } from '@mui/material'
+import { Box, Container, LinearProgress, Button, Grid, Typography, List, ListItem, IconButton, ListItemAvatar, ListItemText, Avatar } from '@mui/material'
 import NewsCard from '../components/NewsCard.jsx'
 import { useScrollStopwatch } from '../hooks/useScrollStopwatch.jsx'
 import useScrollableComponent from '../hooks/useScrollableComponent.jsx'
+import DeleteIcon from '@mui/icons-material/Delete';
+import FolderIcon from '@mui/icons-material/Folder';
 
 const MainPage = () => {
     const { news, loading, loadMore, subscribeSubreddits, unsubscribeSubreddits } = useNewsClient()
@@ -39,10 +41,40 @@ const MainPage = () => {
                 spacing={30}
             >
                 <ScrollTargetComponent />
-                <Container sx={{ mb: 1 }}>
-                    <Button variant='contained' sx={{ mr: 1 }} onClick={() => subscribeSubreddits(['lithuania', 'cryptoCurrency'])}>Subscribe</Button>
-                    <Button variant='contained' onClick={() => unsubscribeSubreddits(['lithuania'])}>Unsubscribe</Button>
-                </Container>
+                <Grid item xs={12} md={6}>
+                    <Container sx={{flexDirection: "row"}}>
+                        <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+                            Avatar with text and icon
+                        </Typography>
+                        <Container sx={{ mb: 1 }}>
+                            <Button variant='contained' sx={{ mr: 1 }} onClick={() => subscribeSubreddits(['lithuania', 'cryptoCurrency'])}>Subscribe</Button>
+                            <Button variant='contained' onClick={() => unsubscribeSubreddits(['lithuania'])}>Unsubscribe</Button>
+                        </Container>
+                    </Container>
+                    <List dense={true}>
+                        {[1, 2, 3].map((value) => (
+                            <ListItem
+                                key={value}
+                                secondaryAction={
+                                    <IconButton edge="end" aria-label="delete">
+                                        <DeleteIcon />
+                                    </IconButton>
+                                }
+                            >
+                                <ListItemAvatar>
+                                    <Avatar>
+                                        <FolderIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary="Single-line item"
+                                    secondary={true ? 'Secondary text' : null}
+                                />
+                            </ListItem>))
+                        }
+                    </List>
+                </Grid>
+
                 {news.map(news =>
                     <NewsCard
                         sx={{ mb: 1 }}
